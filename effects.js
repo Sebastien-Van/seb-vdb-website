295e114eb74e20cd11bfc0abfded1f743f75d236
Sebastien-Van <seb.vdb4@gmail.com>
2022-04-04 18:52:06 -0700

    finished product

--- effects.js
+++ effects.js
@@ -1,3 +1,4 @@
+// declare my variables
 let x = 0;
 let img;
 let cnv;
@@ -5,17 +6,18 @@ let y = 0;
 let dim = 80;
 let width = 1200;
 
-
+//preload function
 function preload(){
   img = loadImage('img/plane.png');
 }
+//make a canvas and set up image
 function setup() {
   cnv=createCanvas(1200, 80);
   image(img, 0 ,0);
   noStroke();
  
 }
-
+//loop function for my image
 function draw(){
    background(253, 235, 249);
   // Animate by increasing value of x
